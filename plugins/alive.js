9c3208f5845e3a02510d189305b1fe6bd21b6f13
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-08-17 11:13:29 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,7 +2,8 @@ module.exports = {
   name: "alive",
   category: "ignore",
   desc: "Is bot alive"
-  } async mbb({ msg }) {
+  } 
+{
      await conn.sendMessage(msg.from, {
           text: ("I AM ALIVE"),
           }});
