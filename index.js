e0daf41bfa1e2813bcf794ada7abfaea8da56c20
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-08-18 18:29:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let config = require('./config')
+const path = require("path");
+let ssname = path.join(__dirname, `./database/session.json`)
+const {StartMillie,MakeSession}= require('./lib')
+const fs = require('fs')
+if(!fs.existsSync(ssname)){
+    MakeSession(config.Session_Id,ssname)
+    }
+
+console.log("ANJALI STARTING...");
+
+StartMillie("./lib");
