9dc6e0da98ac38bc5c60c5ac777d3332668a2b20
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-08-18 15:28:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+// Özel Fonksiyonlarımız
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+
 module.exports = {
   owner: process.env.SUDO || "918113921898,0",
   thumbvideo: process.env.THUMBNAIL_VIDEO || "https://i.imgur.com/2kAp0f0.mp4",
@@ -8,7 +21,7 @@ module.exports = {
   bot_name: process.env.BOT_NAME || "Millie-MD",
   timezone: process.env.TIME_ZONE || "Asia/Kolkata",
   locale: "in",
-  Session_Id: process.env.SESSION_ID || "U2_Fs_dG_Vk_X1_8L_Jr_3k_/3_HG_M4_ce_1d_1g_m+_IC_mR_Hc_5C_x9_kN_E;millie",
+  Session_Id: process.env.SESSION_ID || "U2_Fs_dG_Vk_X1_8L_Jr_3k_/3_HG_M4_ce_1d_1g_m+_IC_mR_Hc_5C_x9_kN_E;anjali",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME||'millietest',
   HEROKU_API_KEY: process.env.HEROKU_API_KEY||'23dfcbe6-d1a1-4b17-a94b-ce9c3dbbc9d7',
   DB_URL:process.env.DATABASE_URL,
