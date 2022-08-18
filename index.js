92c56c087d5366dc8de4baf845065d4b40eac80b
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-08-18 15:48:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,97 @@
-let config = require('./config')
+const fs = require("fs");
 const path = require("path");
-let ssname = path.join(__dirname, `./database/session.json`)
-const {StartMillie,MakeSession}= require('./lib')
-const fs = require('fs')
-if(!fs.existsSync(ssname)){
-    MakeSession(config.Session_Id,ssname)
+const events = require("./events");
+
+const raganork = require("./raganork");
+const liza = require('./Meenu');
+const chalk = require('chalk');
+const config = require('./config');
+const simpleGit = require('simple-git');
+const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
+const { StringSession } = require('./lib');
+const { DataTypes } = require('sequelize');
+const git = simpleGit();
+const axios = require('axios');
+const got = require('got');
+
+const Language = require('./language');
+const Lang = Language.getString('updater');
+
+// Sql
+const WhatsAsenaDB = config.DATABASE.define('WhatsAsena', {
+    info: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    value: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+async function whatsAsena () {
+    await config.DATABASE.sync();
+    var StrSes_Db = await WhatsAsenaDB.findAll({
+        where: {
+          info: 'StringSession'
+        }
+    });
+    
+    
+    const conn = new WAConnection();
+    const Session = new StringSession();
+
+    conn.logger.level = config.DEBUG ? 'debug' : 'warn';
+    var nodb;
+
+    if (StrSes_Db.length < 1) {
+        nodb = true;
+        conn.loadAuthInfo(Session.deCrypt(config.Session_Id)); 
+    } else {
+        conn.loadAuthInfo(Session.deCrypt(StrSes_Db[0].dataValues.value));
     }
 
-console.log("ANJALI STARTING...");
+    conn.on ('credentials-updated', async () => {
+        console.log(
+            chalk.blueBright.italic(' Login information updated!')
+        );
 
-StartMillie("./lib");
+        const authInfo = conn.base64EncodedAuthInfo();
+        if (StrSes_Db.length < 1) {
+            await WhatsAsenaDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
+        } else {
+            await StrSes_Db[0].update({ value: Session.createStringSession(authInfo) });
+        }
+    })    
+
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
+${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
+    });
+    
+
+    conn.on('open', async () => {
+        console.log(
+            chalk.green.bold('✅ Login successful!')
+        );
+
+        console.log(
+            chalk.green.bold('Meenu mwol working' ));
+          
+	   try {
+        await conn.connect();
+    } catch {
+        if (!nodb) {
+            console.log(chalk.red.bold('Eski sürüm stringiniz yenileniyor...'))
+            conn.loadAuthInfo(Session.deCrypt(config.Session_Id)); 
+            try {
+                await conn.connect();
+            } catch {
+                return;
+            }
+        }
+    }
+}
+
+whatsAsena();
