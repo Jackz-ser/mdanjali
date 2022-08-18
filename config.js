70db1fd275967aac77492cb1f666b08a1b73cb20
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-08-18 17:13:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   thumbImg: process.env.THUMBNAIL_IMAGE || "https://i.imgur.com/DfFk2Wx.jpeg",
   ownername: process.env.OWNER_NAME || "Neeraj-x0",
   prefix: process.env.PREFIX || ".",
-  logs: ToBool(process.env.LOG_MSG) || true,
+  logs: convertToBool(process.env.LOG_MSG) || true,
   bot_name: process.env.BOT_NAME || "Millie-MD",
   timezone: process.env.TIME_ZONE || "Asia/Kolkata",
   locale: "in",
