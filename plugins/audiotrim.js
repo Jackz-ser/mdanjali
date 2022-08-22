e281a4d8266dbaf00f489006300b5d47d25c8c43
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-22 20:19:03 +0530

    Trim : better understanding

--- plugins/audiotrim.js
+++ plugins/audiotrim.js
@@ -4,20 +4,20 @@ module.exports = {
   name: "trim",
   category: "Tools",
   desc: "trims the audio",
-  query: "trim start:end",
+  query: "_Give a start and end time_ \n\n _Example_ : *trim 5:20*",
   isQuoted: true,
   isMedia: {
     isQAudio: true,
   },
   async mbb({ msg, conn }, { q }) {
     let [start, end] = q.split(":");
-    if (!end) return msg.reply("_Invalid syntax_");
+    if (!end) return msg.reply("_FORMAT ERROR_ \n\n _EXAMPLE : trim 5:20_");
     const { quoted, from } = msg;
     let media = await quoted.download();
     let audio = await mp3Cutter(media, start, end);
     await conn.sendMessage(
       from,
-      { audio: audio, mimetype: "audio/mpeg", fileName: `Convert By.mp3` },
+      { audio: audio, mimetype: "audio/mpeg", fileName: `ANJALI MD` },
       { quoted: msg }
     );
   },
