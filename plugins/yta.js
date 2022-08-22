46eba5709760f049c2e281ca3823fd3fbea787ec
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-22 20:40:12 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -3,8 +3,8 @@ module.exports = {
     name: "yta",
 	alias: ["ytmp3","ytaudio"],
 	category: "Downloader",
-	desc: "Yotube To Mp3",
-    query:"Give A url",
+	desc: "YouTube To Mp3",
+    query:"_GIVE AN URL_",
     isUrl: true,
     wait: true,
     async mbb({msg,conn },{q}) {    
