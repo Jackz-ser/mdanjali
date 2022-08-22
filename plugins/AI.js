0c1cc9867601f68c51bb45d4843d67ceb9eca493
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-22 20:10:29 +0530

    Chatbot

--- plugins/AI.js
+++ plugins/AI.js
@@ -1,21 +1,21 @@
 const {tiny} = require('../lib')
 module.exports = {
-	name: "ai",
+	name: "chatbot",
 	category: "Fun",
 	desc: "AI Chatbot",
     isOwner:true,
     async mbb({ msg,conn},{q,prefix}) {
         await conn.sendMessage(msg.from, {
-            text: tiny("Chatbot Mode"),
+            text: tiny("CHAT BOT MODE"),
             footer: tiny("Turn on/off chatbot"),
             buttons: [
               {
                 buttonId: `${prefix}setvar AI:false`,
-                buttonText: { displayText: tiny("turn off") },
+                buttonText: { displayText: tiny("DISABLE") },
                 type: 1,
               },{
                 buttonId: `${prefix}setvar AI:true`,
-                buttonText: { displayText: tiny("turn on") },
+                buttonText: { displayText: ("ENABLE") },
                 type: 1,
               }
             ],
