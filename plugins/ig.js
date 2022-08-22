b211273a7dbfe0701b7dbb606aab1d0d3dd96df4
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-22 20:33:27 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,14 +1,14 @@
 const {igdl} = require('../lib')
 module.exports = {
     name: "insta",
-	alias: ["ig","instagram"],
+	alias: ["instagram"],
 	category: "Downloader",
 	desc: "Instagram Downloader",
-    query:"Give A url",
+    query:"_ENTER AN URL_",
     isUrl: true,
     wait: true,
     async mbb({msg,conn },{q}) {    
-        if (!q.includes('www.instagram.com')) return msg.reply("Invalid Link")
+        if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
         let url = await igdl(q)
        let buff = await conn.getBuffer(url[0].url)
           await  conn.sendFile(msg.from, buff,'', "", msg,{quoted:msg})
