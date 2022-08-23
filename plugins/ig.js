745b3827c5c2dc46a51c2244c81ae526bcefacd2
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-23 17:44:38 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -30,7 +30,7 @@ module.exports = {
     wait: true,
     async mbb({msg,conn },{q}) {    
         if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
-        let fetch = await fetchUrl(config.api("zenz", "/downloader/instagram", { url(text)[0] }, "apikey"))
+        let fetch = await fetchUrl(https://zenzapis.xyz/downloader/instagram$url(text)[0]25a71023e0))
         for (let i of fetch.result) conn.sendFile(msg.from, i, "", msg, { caption: `Download Media From : ${url(text)[0]}` })
        { quoted:msg }
     }
