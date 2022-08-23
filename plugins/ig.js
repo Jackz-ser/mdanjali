984907c3edc8c5260f987190a8ac75b68e073ad4
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-23 17:47:34 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,22 +1,3 @@
-/*
-const {instagram} = require('../lib')
-const config = require('./config')
-module.exports = {
-    name: "insta",
-	alias: ["instagram"],
-	category: "Downloader",
-	desc: "Instagram Downloader",
-    query:"_ENTER AN URL_",
-    isUrl: true,
-    wait: true,
-    async mbb({msg,conn },{q}) {    
-        if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
-        let url = await instagram(q)
-       let buff = await conn.getBuffer(url[0].url)
-          await  conn.sendFile(msg.from, buff,'', "", msg,{quoted:msg})
-    }
-}
-*/
 
 const {fetchUrl} = require('../lib')
 const config = require('./config')
@@ -30,7 +11,7 @@ module.exports = {
     wait: true,
     async mbb({msg,conn },{q}) {    
         if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
-        let fetch = await fetchUrl(`https://zenzapis.xyz/downloader/instagram=$url(text)[0]&25a71023e0`)
+        let fetch = await fetchUrl(`https://zenzapis.xyz/downloader/instagram=${url(text)[0]}&25a71023e0`)
         for (let i of fetch.result) conn.sendFile(msg.from, i, "", msg, { caption: `Download Media From : ${url(text)[0]}` })
        { quoted:msg }
     }
