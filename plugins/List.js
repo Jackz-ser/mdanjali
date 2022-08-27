9324c1ec1a97017c1e5d9cc0d0862c354fabac62
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-08-27 18:49:07 +0530

    Create List.js

---
+++ plugins/List.js
@@ -0,0 +1,49 @@
+const config = require('../config')
+module.exports = {
+    name: "list",
+    alias: ["listall"],
+    desc: "List all command",
+    type: "main",
+    async mbb({ msg, conn }, { q, map, prefix }) {
+        if (q) {
+            let data = []
+            let name = q.toLowerCase()
+            let { command, prefix } = map
+            let cmd = commands.get(name) || Array.from(commands.values()).find((v) => v.alias.includes(name))
+            if (!cmd || cmd.type == "hidden") return msg.reply("No Command Found")
+            else data.push(`*Name :* ${cmd.name.replace(/^\w/, c => c.toUpperCase())}`)
+            if (cmd.alias) data.push(`*Alias :* ${cmd.alias.join(", ")}`)
+            if (cmd.use) data.push(`*Use:* ${cmd.use}`);
+            if (cmd.desc) data.push(`*Description :* ${cmd.desc}\n`)
+            if (cmd.example) data.push(`*Example :* ${cmd.example.replace(/%prefix/gi, prefix).replace(/%command/gi, cmd.name).replace(/%text/gi, text)}`)
+            return msg.reply(`*Info Command ${cmd.name.replace(/^\w/, c => c.toUpperCase())}*\n\n${data.join("\n")}`)
+        } else {
+            let teks = `Hello, ${msg.pushName} \nHere is the Command List\n\n`
+
+            for (let type of commands.type) {
+                teks += `┌──⭓ *${toUpper(type)} Menu*\n`
+                teks += `│\n`
+                teks += `${commands.list[type].filter(v => v.type !== "hidden").map((cmd) => `│⭔ ${prefix + cmd.name} ${cmd.use ? " " + cmd.use : ""}`).join("\n")}\n`
+                teks += `│\n`
+                teks += `└───────⭓\n\n`
+            }
+
+            teks += `Send ${prefix}help followed by a command name to get detail of command, ex: ${prefix}help sticker`;
+            let templateButtons = [
+                { urlButton: { displayText: "Source Code", url: "https://instgram.com/safxr._" } },
+                { urlButton: { displayText: "Main APIs", url: "http://zenzapis.xyz" } },
+                { quickReplyButton: { displayText: "ping", id: "${prefix} ping" } },
+                { quickReplyButton: { displayText: "Runtime", id: "${prefix} runtime" } },
+                { quickReplyButton: { displayText: "Button 3", id: "nothing" } },
+            ]
+            
+            let templateMessage = {
+                image: { url: config.thumbimage },
+                caption: teks,
+                footer: "List test",
+                templateButtons: templateButtons
+            }
+
+            conn.sendMessage(msg.from, templateMessage, { quoted: msg })
+        }
+    },
