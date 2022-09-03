5d0f5d1ce9225e0aa836aed9d48c71b2c783e5ac
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-09-03 14:21:21 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -15,8 +15,8 @@ module.exports = {
         for (let { downloadUrl, type } of json) {
           conn.sendFile(msg.from, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), {quoted:msg})
             }
-        )}
-    }
+        }
+    )}
 }
 
 
