86517f51fa56a014dfb5de53ef253eedd8ceee32
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-09-03 12:02:23 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -10,13 +10,23 @@ module.exports = {
     async mbb({msg,conn },{q}) {    
         if (!q.includes('www.instagram.com')) return msg.reply("Invalid Link")
         let url = await igdl(q)
-       let buff = await conn.getBuffer(url[0].url)
-          await conn.sendFile(msg.from, buff,'', "", msg,{quoted:msg})
+        let igdl = JSON.stringify(res)
+        let json = JSON.parse(igdl)
+        for (let { downloadUrl, type } of json) {
+          conn.sendFile(msg.from, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), {quoted:msg})
     }
 }
 
 
 /*
+igdl(args[0]).then(async res => {
+    let igdl = JSON.stringify(res)
+    let json = JSON.parse(igdl)
+    await m.reply(global.wait)
+    for (let { downloadUrl, type } of json) {
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), {quoted:msg})
+    }
+
 try{
     if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
     await conn.sendMessage("_*DOWNLOADING...*_")
