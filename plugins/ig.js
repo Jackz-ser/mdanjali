fc268c3d35a5bfa3e2058fde617e8512959a7ecf
JACKZ <96305513+Jackz-ser@users.noreply.github.com>
2022-09-20 21:53:05 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,60 +1,30 @@
-/*
-const {igdl} = require('../lib')
+const igdl = require('@bochilteam/scraper')
 module.exports = {
-    name: "insta",
-	alias: ["haha"],
-	category: "Downloader",
+      name: "insta",
+        category: "Downloader",
 	desc: "Instagram Downloader",
     query:"Give A url",
     isUrl: true,
     wait: true,
-    async mbb({msg,conn },{q}) {    
-        if (!q.includes('www.instagram.com')) return msg.reply("Invalid Link")
-        let url = await igdl(q)
-       let buff = await conn.getBuffer(url[0].url)
-          await conn.sendFile(msg.from, buff,'', "", msg,{quoted:msg})
-    }
-}
+    async mbb({msg,conn},{q}) {    
+if (!q.includes('instagram.com')) return msg.reply("Enter an Instagram link")
 
-const {igdl} = require('../lib/scrapers')
-module.exports = {
-    name: "ig",
-	alias: ["hehe"],
-	category: "Downloader",
-	desc: "Instagram Downloader",
-    query:"Give A url",
-    isUrl: true,
-    wait: true,
-    async mbb({msg,conn },{q}) {    
-        if (!q.includes('www.instagram.com')) return msg.reply("Invalid Link")
-        let url = await igdl(q)
-       let buff = await conn.getBuffer(url[0].url)
-          await conn.sendFile(msg.from, buff,'', "", msg,{quoted:msg})
-    }
-}
+    igdl.savefrom(q)
 
+    .then(async(result) => {	  	                                	                      	            
 
+  for(let i of result.url) {		
 
-igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
-    await m.reply(global.wait)
-    for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), {quoted:msg})
+  if(i.url.includes('mp4')){		           			    				
+
+let link = await getBuffer(i.url)
+
+      await conn.sendMessage(msg.chat, { video: link, caption: `*INSTAGRAM DOWNLOADER*` }, { quoted: msg })                  
+
+     }
+
     }
 
-try{
-    if (!q.includes('www.instagram.com')) return msg.reply("*INVALID LINK*")
-    await conn.sendMessage("_*DOWNLOADING...*_")
-    res = await igdl(match[1])
-    ytm = res.result
-    let buff = await conn.getBuffer(`${ytm.link}`)
-    const insta = `${ytm.link}`
-    await conn.sendFile(msg.from, buff, "", msg, { quoted:msg })
-  } 
-  catch
-  {
-     await conn.sendMessage("error")
-  }
-}};
-*/
+  }).catch((err) => sendReply(`*Sorry, server error*`))
+
+  }		
